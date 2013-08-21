58422d6ddbe88f427c9533ed0ba8b0db3e29fafc
Visa Putkinen <visa.putkinen@reaktor.fi>
2013-08-21 05:09:01 +0300

    Core functionality complete, but untested

--- src/me/vpu/wifiautorebooter/WiFiAutoRebooterReceiver.java
+++ src/me/vpu/wifiautorebooter/WiFiAutoRebooterReceiver.java
@@ -1,25 +1,85 @@
 package me.vpu.wifiautorebooter;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.util.Calendar;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.util.Log;
 
 public class WiFiAutoRebooterReceiver extends BroadcastReceiver {
+	private static final String LOG_TAG = WiFiAutoRebooterReceiver.class.getName();
+	private static final int checkIntervalInSeconds = 30;
+	private static String ROUTER_IP_KEY = "routerIP";
+	private static String DEFAULT_ROUTER_IP = "192.168.0.1";
+	
 	@Override
-	public void onReceive(Context arg0, Intent arg1) {
-		Log.d(WiFiAutoRebooterStarter.LOG_TAG, "WiFiAutoRebooterReceiver.onReceive()");
-		try {
-			InetAddress router = InetAddress.getByName("192.168.0.1");
-			boolean isReachable = router.isReachable(300);
-			Log.d(WiFiAutoRebooterStarter.LOG_TAG, "Router reachable: "+isReachable);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (Throwable e) {
-			e.printStackTrace();
+	public void onReceive(Context mCtx, Intent intent) {
+		Log.d(LOG_TAG, "WiFiAutoRebooterReceiver.onReceive()");
+
+		String routerIP = intent.getExtras().getString(ROUTER_IP_KEY);
+		routerIP = routerIP==null ? DEFAULT_ROUTER_IP : routerIP;
+		new CheckRouterConnectivityTask(mCtx, routerIP).execute();
+
+		scheduleNextWiFiCheck(mCtx, routerIP, checkIntervalInSeconds * 1000);
+	}
+	
+	private void onRouterConnectivityCheckComplete(Context mCtx, String routerIP, boolean routerReachable) {
+		Log.d(LOG_TAG, "Router "+routerIP+" reachable: "+routerReachable);
+		if (!routerReachable) {
+			WifiManager ws = (WifiManager)mCtx.getSystemService(Context.WIFI_SERVICE);
+			boolean reassociationSucceeded = ws.reassociate();
+			Log.d(LOG_TAG, "Reassociation succeeded: "+reassociationSucceeded);
 		}
+	}
+	
+	public static void scheduleNextWiFiCheck(Context mCtx, String routerIP, long delayInMillis) {
+		Intent alarmIntent = new Intent(mCtx, WiFiAutoRebooterReceiver.class);
+		alarmIntent.putExtra(ROUTER_IP_KEY, routerIP);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(mCtx, 0, alarmIntent,
+				PendingIntent.FLAG_UPDATE_CURRENT);
+		AlarmManager alarmManager = (AlarmManager)mCtx.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.set(AlarmManager.RTC, Calendar.getInstance().getTimeInMillis() + delayInMillis, pendingIntent);
+	}
+	
+	
+	private class CheckRouterConnectivityTask extends AsyncTask<Void, Void, Boolean> {
+		Context mCtx;
+		String routerIP;
+		
+		public CheckRouterConnectivityTask(Context mCtx, String routerIP) {
+			super();
+			this.mCtx = mCtx;
+			this.routerIP = routerIP;
+		}
+		
+		@Override
+		protected Boolean doInBackground(Void... args) {
+			try {
+				InetAddress router = InetAddress.getByName(routerIP);
+				boolean isReachable = router.isReachable(1000);
+				return isReachable;
+			} catch (Throwable t) {
+				StringWriter sw = new StringWriter();
+				PrintWriter pw = new PrintWriter(sw);
+				t.printStackTrace(pw);
+				Log.e(LOG_TAG, sw.toString());
+			}
+			return false;
+		}
+		
+		@Override
+		protected void onPostExecute(Boolean result) {
+			super.onPostExecute(result);
+			onRouterConnectivityCheckComplete(mCtx, routerIP, result);
+		}
+	
 	}
 }
--- src/me/vpu/wifiautorebooter/WiFiAutoRebooterStarter.java
+++ src/me/vpu/wifiautorebooter/WiFiAutoRebooterStarter.java
@@ -1,21 +1,17 @@
 package me.vpu.wifiautorebooter;
 
-import java.util.Calendar;
-
 import android.app.Activity;
-import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 public class WiFiAutoRebooterStarter extends Activity {
-	static final String LOG_TAG = "WiFiAutoRebooter";
-	static final int NOTIFICATION_ID = 0;
+	private static final String LOG_TAG = WiFiAutoRebooterStarter.class.getName();
+	private static final int NOTIFICATION_ID = 0;
+	private static final String ROUTER_IP = "192.168.0.1";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +19,14 @@ public class WiFiAutoRebooterStarter extends Activity {
 		Log.d(LOG_TAG, "WiFiAutoRebooterStarter.onCreate()");
 		
 		addNotificationIcon();
-		startReceiver();
+		WiFiAutoRebooterReceiver.scheduleNextWiFiCheck(getApplicationContext(), ROUTER_IP, 0);
 		finish();
 	}
 	
 	private void addNotificationIcon() {
 		Notification n = new Notification();
 		n.icon = R.drawable.ic_launcher;
-		n.tickerText = "WiFiAutoRebooter running...";
+		n.tickerText = getResources().getString(R.string.notification_text);
 		n.contentView = new RemoteViews("me.vpu.wifiautorebooter", R.layout.notificationlayout);
 		n.flags = Notification.FLAG_NO_CLEAR;
 		
@@ -38,13 +34,5 @@ public class WiFiAutoRebooterStarter extends Activity {
 				(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 		// mId allows you to update the notification later on.
 		mNotificationManager.notify(NOTIFICATION_ID, n);
-	}
-	
-	private void startReceiver() {
-		Intent alarmIntent = new Intent(getApplicationContext(), WiFiAutoRebooterReceiver.class);
-		PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, alarmIntent,
-				PendingIntent.FLAG_UPDATE_CURRENT);
-		AlarmManager alarmManager = (AlarmManager)getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-		alarmManager.set(AlarmManager.RTC, Calendar.getInstance().getTimeInMillis(), pendingIntent);
 	}
 }
